f6ade17aa615a031a777e0a170e55da9aecc4ab5
Neramit <DavidTh30@gmail.com>
2026-07-06 19:08:13 +0700

    Add files via upload

--- DDE01_Server/unit1.pas
+++ DDE01_Server/unit1.pas
@@ -6,7 +6,7 @@ interface
 
 uses
   Classes, SysUtils, Forms, Controls, Graphics, Dialogs, StdCtrls,
-  ExtCtrls, Windows, Messages, dbugintf;
+  ExtCtrls, Windows, Messages, dbugintf, StrUtils;
 
 type
 
